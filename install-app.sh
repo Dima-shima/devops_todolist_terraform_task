7658e3bab91cb10578b66f9e925e2af891ae9877
Dima-shima <shifrin1970@gmail.com>
2026-08-14 19:09:32 +0200

    Fix app installation script

--- install-app.sh
+++ install-app.sh
@@ -8,13 +8,13 @@
 # used to suppress any interactive prompts - we won't be able to confirm operation 
 # when running the script as VM extention.  
 apt-get update -yq
-apt-get install python3-pip -yq
+apt-get install git python3-pip -yq
 
 # Create a directory for the app and download the files. 
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
-cp -r devops_todolist_terraform_task/app/* /app
+git clone https://github.com/Dima-shima/azure_task_12_deploy_app_with_vm_extention.git
+cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
 mv /app/todoapp.service /etc/systemd/system/
